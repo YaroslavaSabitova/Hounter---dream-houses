f322dfab6db721be93b3ef91accbc2a3dbec31d3
unknown <slava.sabitova@yandex.ru>
2023-04-23 23:22:39 +0300

    change slider and review

--- js/script.js
+++ js/script.js
@@ -1,76 +1,19 @@
-"use strict"
+// section features
 
-const SLIDE_WIDTH = 29;
-
-const navItems = document.querySelectorAll('.slider__navigation-link');
-const slides = document.querySelector('.slides');
-const slideList = document.querySelectorAll('.slide');
-const buttonPrev = document.querySelector('.slider__button_prev');
-const buttonNext = document.querySelector('.slider__button_next');
-
-let currentSlide = '1';
-
-const setSlideActive = () => {
-  slides.style.transform = `translate3d(${- (currentSlide - 1) * SLIDE_WIDTH}%, 0, 0)`;
-};
-
-const setActiveNavItem = () => {
-  navItems.forEach((item) => {
-    item.classList.toggle('active', item.getAttribute('href') === '#' + 'slide-' + currentSlide);
-  })
-};
-
-const setActiveButtons = () => {
-  buttonPrev.toggleAttribute('disabled', currentSlide <= 1);
-
-  buttonNext.toggleAttribute('disabled', currentSlide >= slideList.length);
-};
-
-const navItemClickHandler = (evt) => {
-  evt.preventDefault();
-  const item = evt.target;
-
-  const anchor = item.getAttribute('href');
-  if (!anchor) {
-    return;
-  }
-
-  currentSlide = anchor.substring(7);
-
-  setSlideActive();
-  setActiveButtons();
-  setActiveNavItem();
-};
-
-const buttonPrevClickHandler = () => {
-  currentSlide--;
-
-  setSlideActive();
-  setActiveButtons();
-  setActiveNavItem();
-};
-
-const buttonNextClickHandler = () => {
-  currentSlide++;
-
-  setSlideActive();
-  setActiveButtons();
-  setActiveNavItem();
-};
-
-const init = () => {
-  navItems.forEach((item) => {
-    item.addEventListener('click', navItemClickHandler);
-  });
-
-  buttonPrev.addEventListener('click', buttonPrevClickHandler);
-
-  buttonNext.addEventListener('click', buttonNextClickHandler);
-};
-
-init();
+const swiper1 = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  centeredSlides: true,
+  initialSlide: 1,
+});
 
 
+
+// section review
 
 const swiper = new Swiper(".mySwiper", {
   pagination: {
@@ -82,3 +25,5 @@ const swiper = new Swiper(".mySwiper", {
   centeredSlides: true,
   initialSlide: 1,
 });
+
+
