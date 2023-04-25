12aca035f1fc6103d1215d6ea101ee495252522f
unknown <slava.sabitova@yandex.ru>
2023-04-25 18:50:25 +0300

    media queries part 3 mobile version

--- js/script.js
+++ js/script.js
@@ -11,8 +11,6 @@ const swiper1 = new Swiper(".mySwiper1", {
   initialSlide: 1,
 });
 
-
-
 // section review
 
 const swiper = new Swiper(".mySwiper", {
@@ -26,4 +24,23 @@ const swiper = new Swiper(".mySwiper", {
   initialSlide: 1,
 });
 
+// hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.mobile-menu'),
+  menuItem = document.querySelectorAll('.mobile-menu__list-item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('mobile-menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('mobile-menu_active');
+      })
+  })
+})
 
